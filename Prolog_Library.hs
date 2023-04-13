a07196a0fc4881207f4730fd5ffd6138be1f8048
Ari Vitor S. Lazzarotto <arivitordasilva@gmail.com>
2023-04-13 14:35:37 -0300

    Commutative property problem resolved.

--- Prolog_Library.hs
+++ Prolog_Library.hs
@@ -32,15 +32,15 @@ match (Atom x) (Atom y) = x == y
 match (Var x) _ = True
 match _ (Var x) = True
 match (Var t :\== Var t') _ = False
-match (Func n1 args1) (Func n2 args2) = n1 == n2 && length args1 == length args2 && all (uncurry match) (zip args1 args2)          --Verifica se é a mesma função e se os argumentos podem ser unificados.                       
+match (Func n1 args1) (Func n2 args2) = n1 == n2 && length args1 == length args2 && all (uncurry match) (zip args1 args2)          --Verifica se é a mesma função e se os argumentos podem ser unificados.                      
 match _ _ = False
 
--- Substitui variáveis em um termo
+-- Substitui uma variável por um termo em um outro termo passado como argumento e retorna o termo substituído.
 substitute :: (String, Term) -> Term -> Term
-substitute (x, t) (Var y) = if x == y then t else Var y                                             --Se a variável for igual a x, retorna o termo t, senão retorna a variável.
 substitute (x, t) (Atom y) = Atom y
-substitute (x, t) (Func n args) = Func n (map (substitute (x, t)) args)                             --Aplica a substituição em toda a lista de argumentos.
+substitute (x, t) (Var y) = if x == y then t else Var y                                             --Se a variável for igual a x, retorna o termo t, senão retorna a variável.
 substitute (x, t) (Var a :\== Var b) = if x == a then Var b :\== Var b else Var a :\== Var b        --Substitui a variável em uma expressão de desigualdade.
+substitute (x, t) (Func n args) = Func n (map (substitute (x, t)) args)                             --Aplica a substituição em toda a lista de argumentos.
 
 substituteAll :: [(String, Term)] -> Term -> Term
 substituteAll [] t = t
@@ -63,13 +63,17 @@ unify (Atom x) (Atom y) = if x == y then Just [] else Nothing                   
 unify (Var x) (Atom y) = Just [(x, Atom y)]                                                          --Se forem uma variável e um átomo, retorna a substituição.
 unify (Atom x) (Var y) = Just [(y, Atom x)]                                                          --Se forem um átomo e uma variável, retorna a substituição.
 unify (Var x) (Var y) = Just [(x, Var y)]                                                            --Se forem variáveis, retorna a substituição, pois variaveis sempre podem ser unificadas.
-unify (Var t :\== Var t') _ = if t == t' then Just [] else Nothing
-unify t (Var x) = unify (Var x) t
 unify (Var x) t
        | x `notElem` vars t = Just [(x, t)]                                                          --Se x não estiver em t, retorna a substituição.
        | otherwise = case freshVars (vars t) of                                                      --Se x já estiver em t, gera um novo nome de variável antes.
-              [] -> Nothing 
+              [] -> Nothing
               (y:_) -> Just [(y, t)]                                                                 --Usa o novo nome de variável para substituir x em t.
+unify t (Var y)
+       | y `notElem` vars t = Just [(y, t)]
+       | otherwise = case freshVars (vars t) of
+              [] -> Nothing
+              (x:_) -> Just [(x, t)]
+unify (Var t :\== Var t') _ = if t == t' then Just [] else Nothing
 unify (Func n1 args1) (Func n2 args2) = if n1 == n2 && length args1 == length args2 then unifyList args1 args2 else Nothing--Se as funções tiverem o mesmo nomee o mesmo número de argumentos, tenta unificar as listas de argumentos.
 unify _ _ = Nothing
 
@@ -84,13 +88,14 @@ unifyList _ _ = Nothing
 
 -- Interpreta o programa, percorre cada cláusula do programa e gera uma lista de substituições a serem realizadas.
 interpret :: Prolog -> Term -> Subst
-interpret prog term = case unify (headOf clause) term of                                              --Unifica a cabeça da cláusula com o termo.
-       Nothing -> Nothing
-       Just subst -> case interpretList prog (map (substituteAll subst) (bodyOf clause)) of           --Caso unifique, aplica a substituição no corpo da cláusula.
+interpret prog term = case query prog term of
+       [] -> Nothing
+       (clause:_) -> case unify (headOf clause) term of
               Nothing -> Nothing
-              Just subst' -> Just (subst ++ subst')
-       where clause = head (query prog term)                                                          --Busca a primeira cláusula que unifica com o termo.
-             query prog term = [clause | clause <- prog, match term (headOf clause)]                  --Verifica se a cabeça da cláusula? unifica com o termo para toda cláusula do programa e retorna a lista de cláusulas que unificam com o termo - -- busca cláusulas que unificam com um termo. unify (headOf (head (query myExample3 (Func "irmao" [Var "X", Atom "ariel"])))) (Func "irmao" [Var "X", Atom "ariel"])
+              Just subst -> case interpretList prog (map (substituteAll subst) (bodyOf clause)) of
+                     Nothing -> Nothing
+                     Just subst' -> Just (subst ++ subst')
+       where query prog term = [clause | clause <- prog, match term (headOf clause)]
 
 interpretList :: Prolog -> [Term] -> Subst
 interpretList _ [] = Just []
@@ -100,22 +105,28 @@ interpretList prog (t:ts) = case interpret prog t of
               Nothing -> Nothing
               Just subst' -> Just (subst ++ subst')
 
--- Aplica a substituição em todas as variáveis da lista subst
+-- Aplica a substituição em todas as variáveis da lista subst e retorna o resultado.
 result :: Subst -> [Term]
 result Nothing = []
 result (Just subst) = [substituteAll subst (Var x) | (x, _) <- subst]                                --Aplica a substituição em todas as variáveis da lista subst.
 
--- Retorna o resultado da consulta
+-- Retorna o resultado da consulta, caso não encontrar, inverte a lista de argumentos do termo e tenta novamente.
 queryResult :: Prolog -> Term -> [Term]
-queryResult prog term = case interpret prog term of 
-       Nothing -> []                                        
+queryResult prog term = case interpret prog term of
        Just subst -> [head (result (Just subst))]
+       Nothing -> case term of
+              Func n xs -> queryResultAux prog (Func n (reverse xs))                                 --Inverte a lista de argumentos do termo, necessita ser uma função auxiliar para não entrar em loop infinito.
+              _ -> []
+       where  
+              queryResultAux prog term = case interpret prog term of  
+                     Just subst -> [head (result (Just subst))]
+                     Nothing -> []
 
 main1 :: IO ()
 main1 = print (queryResult myExample1 (Func "parent" [Var "X", Atom "koos"]))
 
 main2 :: IO ()
-main2 = print (queryResult myExample2 (Func "likes" [Var "X", Atom "prolog"]))
+main2 = print (queryResult myExample2 (Func "likes" [Atom "prolog", Var "X"]))
 
 main3 :: IO ()
 main3 = print (queryResult myExample3 (Func "irmao" [Var "X", Atom "ariel"]))
@@ -138,7 +149,7 @@ myExample2 = [
        Simple (Func "based" [Atom "haskell", Atom "maths"]),
        Simple (Func "likes" [Atom "max", Atom "logic"]),
        Simple (Func "likes" [Atom "claire", Atom "maths"]),
-       Func "likes" [Var "X", Var "P"] :- [Func "based" [Var "P", Var "Y"], Func "likes" [Var "X", Var "Y"]]] 
+       Func "likes" [Var "X", Var "P"] :- [Func "based" [Var "P", Var "Y"], Func "likes" [Var "X", Var "Y"]]]
 
        --Genealogic Tree Example --- olhar maribel, tudo que era x, vira Z, nas substituições
 myExample3 :: Prolog

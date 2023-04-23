17c49546a921b9e667a3a35832c9f4f9c143cdb4
Ari Vitor S. Lazzarotto <arivitordasilva@gmail.com>
2023-04-23 17:37:45 -0300

    2 bugs and one funcionality more.

--- Prolog_Library.hs
+++ Prolog_Library.hs
@@ -1,14 +1,11 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 
-import Data.List (nub)
+import Data.List (nub) -- nub remove elementos duplicados de uma lista.
 
--- Ari Vitor da Silva Lazzarotto
+-- https://downloads.haskell.org/~ghc/7.2.1/docs/html/users_guide/pragmas.html - 7.13.5.1. INLINE pragma - GHC User's Guide
 
---1 - Interpretar com Ghci:
---2 - Digitar "main" e apertar enter no interpretador:
-
-data Term = Var String | Atom String | Func String [Term] | Term :\== Term          -- :\== necessita ser adicionado em match e unify para que o programa funcione corretamente.
+data Term = Var String | Atom String | Func String [Term] | Term :\== Term
        deriving (Eq,Show)
 data Clause = Term :- [Term] | Simple Term
        deriving (Eq,Show)
@@ -16,118 +13,20 @@ data Clause = Term :- [Term] | Simple Term
 type Prolog = [Clause]
 type Subst = Maybe [(String, Term)]
 
--- Retorna a cabeça de uma cláusula
-headOf :: Clause -> Term
-headOf (t :- _) = t
-headOf (Simple t) = t
-
--- Retorna o corpo de uma cláusula
-bodyOf :: Clause -> [Term]
-bodyOf (t :- ts) = ts
-bodyOf (Simple t) = []
-
--- Verifica se dois termos unificam
-match :: Term -> Term -> Bool
-match (Atom x) (Atom y) = x == y
-match (Var x) _ = True
-match _ (Var x) = True
-match (Var t :\== Var t') _ = False
-match (Func n1 args1) (Func n2 args2) = n1 == n2 && length args1 == length args2 && all (uncurry match) (zip args1 args2)          --Verifica se é a mesma função e se os argumentos podem ser unificados.                      
-match _ _ = False
-
--- Substitui uma variável por um termo em um outro termo passado como argumento e retorna o termo substituído.
-substitute :: (String, Term) -> Term -> Term
-substitute (x, t) (Atom y) = Atom y
-substitute (x, t) (Var y) = if x == y then t else Var y                                             --Se a variável for igual a x, retorna o termo t, senão retorna a variável.
-substitute (x, t) (Var a :\== Var b) = if x == a then Var b :\== Var b else Var a :\== Var b        --Substitui a variável em uma expressão de desigualdade.
-substitute (x, t) (Func n args) = Func n (map (substitute (x, t)) args)                             --Aplica a substituição em toda a lista de argumentos.
-
-substituteAll :: [(String, Term)] -> Term -> Term
-substituteAll [] t = t
-substituteAll ((x, t):xs) t' = substituteAll xs (substitute (x, t) t')                              --Aplica a substituição em todos os termos da lista.
-
-generateNewVarName :: String -> [String] -> String
-generateNewVarName x usedVars = if x `elem` usedVars then generateNewVarName (x ++ "'") usedVars else x
-
--- Conversão Alfa - Gera sempre uma nova variável para substituir a variável da consulta. Ex: X = Y, gera uma nova variável Z e substitui todas as ocorrências de X por Z.
-alphaConversion :: Term -> [String] -> Term
-alphaConversion (Atom x) usedVars = Atom x
-alphaConversion (Var x) usedVars = Var (generateNewVarName x usedVars)
-alphaConversion (Func n args) usedVars =
-       let args' = map (`alphaConversion` usedVars) args
-       in Func n args'
-alphaConversion t _ = t
-
--- Retorna todas as substituições que unificam com um termo.
-unify :: Term -> Term -> [String] -> Subst
-unify (Atom x) (Atom y) vars = if x == y then Just [] else Nothing                                        --Se forem átomos, verifica se são iguais e retorna a substituição vazia.
-unify (Var x) (Var y) vars = Just [(x, Var y)]                                                            --Se forem variáveis, retorna a substituição, pois variaveis sempre podem ser unificadas.
-unify t (Var x) vars = Just [(x, t)]                                                                      --Se o termo for uma variável, retorna a substituição.
-unify (Var x) t vars = Just [(x, t)]                                                                      --Se o termo for uma variável, retorna a substituição.
-unify (Var t :\== Var t') _  vars= if t == t' then Just [] else Nothing
-unify (Func n1 args1) (Func n2 args2) vars = if n1 == n2 && length args1 == length args2 then unifyList args1 args2 vars else Nothing--Se as funções tiverem o mesmo nomee o mesmo número de argumentos, tenta unificar as listas de argumentos.
-unify _ _ _= Nothing
-
--- Unifica dois termos aplicando a conversão alfa
-unifyWithAlphaConversion :: Term -> Term -> [String] -> Subst
-unifyWithAlphaConversion t1 t2 vars =
-       let t1' = alphaConversion t1 vars
-           t2' = alphaConversion t2 vars
-       in unify t1' t2' vars
-
-unifyList :: [Term] -> [Term] -> [String] -> Subst
-unifyList [] [] _ = Just []
-unifyList (t:ts) (t':ts') vars = case unifyWithAlphaConversion t t' vars of
-       Nothing -> Nothing
-       Just subst -> case unifyList (map (substituteAll subst) ts) (map (substituteAll subst) ts') vars of
-              Nothing -> Nothing
-              Just subst' -> Just (subst ++ subst')
-unifyList _ _ _ = Nothing
-
--- Interpreta o programa, percorre cada cláusula e gera uma lista de substituições a serem realizadas.
-interpret :: Prolog -> Term -> [String] -> Subst
-interpret prog term vars = case query prog term of
-       [] -> Nothing
-       (clause:_) -> case unifyWithAlphaConversion (headOf clause) term vars of
-              Nothing -> Nothing
-              Just subst -> case interpretList prog (map (substituteAll subst) (bodyOf clause)) vars of
-                     Nothing -> Nothing
-                     Just subst' -> Just (subst ++ subst')
-       where query prog term = [clause | clause <- prog, match term (headOf clause)]
-
--- Interpreta o programa aplicando a conversão alfa em cada cláusula e no termo da consulta.
-interpretWithAlphaConversion :: Prolog -> Term -> [String] -> Subst
-interpretWithAlphaConversion prog term vars =
-       let prog' = applyAlphaConversion prog vars
-           term' = alphaConversion term vars
-       in interpret prog' term' vars
-       where
-              applyAlphaConversion prog vars = map (`applyAlphaConversionClause` vars) prog
-                     where
-                            applyAlphaConversionTerm t vars = alphaConversion t vars
-                            applyAlphaConversionClause (Simple t) vars = Simple (applyAlphaConversionTerm t vars)
-                            applyAlphaConversionClause (head :- body) vars = applyAlphaConversionTerm head vars :- map (`applyAlphaConversionTerm` vars) body
-
-interpretList :: Prolog -> [Term] -> [String] -> Subst
-interpretList _ [] _ = Just []
-interpretList prog (t:ts) vars= case interpretWithAlphaConversion prog t vars of
-       Nothing -> Nothing
-       Just subst -> case interpretList prog (map (substituteAll subst) ts) vars of
-              Nothing -> Nothing
-              Just subst' -> Just (subst ++ subst')
-
--- Aplica a substituição em todas as variáveis da lista subst e retorna o resultado.
-result :: Subst -> [Term]
-result Nothing = []
-result (Just subst) = [substituteAll subst (Var x) | (x, _) <- subst]                                --Aplica a substituição em todas as variáveis da lista subst.
-
 -- Retorna o resultado da consulta, caso não encontrar, tenta novamente para todas permutas da lista.
 queryResult :: Prolog -> Term -> [Term]
 queryResult prog term = case interpretWithAlphaConversion prog term (usedVars prog) of
-       Nothing -> []
-       Just subst -> [head (result (Just subst))]
+       Nothing -> case term of
+              Func n xs -> queryResultAux prog (Func n (reverse xs))                                                --Inverte a lista de argumentos do termo, necessita ser uma função auxiliar para não entrar em loop infinito.
+              _ -> []
+       Just solution -> [head (result (Just solution))]
        where
-              usedVars prog = nub (concatMap usedVarsClause prog)
+              result Nothing = []
+              result (Just subst) = [substituteAll subst (Var x) | (x, _) <- subst]                                 --Desencapsula o Maybe e retorna o resultado.
+              queryResultAux prog term = case interpretWithAlphaConversion prog term (usedVars prog) of
+                     Just subst -> [head (result (Just subst))]
+                     Nothing -> []
+              usedVars prog = nub (concatMap usedVarsClause prog) ++ nub (usedVarsTerm term)                        --Retorna uma lista com todas as variáveis usadas no programa e no termo da consulta.
                      where
                             usedVarsClause (t :- ts) = usedVarsTerm t ++ concatMap usedVarsTerm ts
                             usedVarsClause (Simple t) = usedVarsTerm t
@@ -136,16 +35,113 @@ queryResult prog term = case interpretWithAlphaConversion prog term (usedVars pr
                             usedVarsTerm (Var a :\== Var b) = [a, b]
                             usedVarsTerm (Func n args) = concatMap usedVarsTerm args
 
+-- Invoca a função interpret com a conversão alfa aplicada em cada cláusula do programa e no termo da consulta.
+interpretWithAlphaConversion :: Prolog -> Term -> [String] -> Subst
+interpretWithAlphaConversion prog term usedVars =
+       let prog' = applyAlphaConversion prog usedVars
+           term' = alphaConversion term usedVars
+       in interpret prog' term' usedVars
+       where
+              alphaConversion (Atom x) usedVars = Atom x                                                                        -- alphaConversion (Func "consulta" [Atom "ariel", Var "P"]) ["X", "P"]
+              alphaConversion (Var x) usedVars = Var (generateNewVarName x usedVars)
+                     where
+                            generateNewVarName var usedVars = if var `elem` usedVars then generateNewVarName (var ++ "'") usedVars else var--Gera uma nova variável para substituir a variável da consulta. Ex: X = Y, gera uma nova variável Z e substitui todas as ocorrências de X por Z.
+              alphaConversion (Func n args) usedVars = let args' = map (`alphaConversion` usedVars) args in Func n args'--Aplica a conversão alfa em cada argumento da função. Retorna a função com os argumentos convertidos.
+              alphaConversion t _ = t
+              applyAlphaConversion prog usedVars = map (`applyAlphaConversionClause` usedVars) prog
+                     where
+                            applyAlphaConversionTerm t usedVars = alphaConversion t usedVars
+                            applyAlphaConversionClause (Simple t) usedVars = Simple (applyAlphaConversionTerm t usedVars)
+                            applyAlphaConversionClause (head :- body) usedVars = applyAlphaConversionTerm head usedVars :- map (`applyAlphaConversionTerm` usedVars) body
+
+-- Percorre cada cláusula e aplica a unificação com o termo da consulta.
+interpret :: Prolog -> Term -> [String] -> Subst
+interpret prog term usedVars = case query prog term of
+       [] -> Nothing
+       (clause:_) -> case unify (headOf clause) term of
+              Nothing -> Nothing
+              Just subst -> case interpretList prog (map (substituteAll subst) (bodyOf clause)) usedVars of
+                     Nothing -> Nothing
+                     Just subst' -> Just (subst ++ subst')
+       where
+              interpretList _ [] _ = Just []
+              interpretList prog (t:ts) usedVars = case interpretWithAlphaConversion prog t usedVars of--Recursão que percorre cada termo da lista de termos da cláusula e aplica a unificação com o termo da consulta
+                     Nothing -> Nothing
+                     Just subst -> case interpretList prog (map (substituteAll subst) ts) usedVars of
+                            Nothing -> Nothing
+                            Just subst' -> Just (subst ++ subst')
+              headOf (t :- _) = t
+              headOf (Simple t) = t
+              bodyOf (t :- ts) = ts
+              bodyOf (Simple t) = []
+              query prog term = [clause | clause <- prog, match term (headOf clause)]
+                     where
+                            match (Atom x) (Atom y) = x == y
+                            match (Var x) _ = True
+                            match _ (Var x) = True
+                            match (Var t :\== Var t') _ = t /= t'
+                            match (Func n1 args1) (Func n2 args2) = n1 == n2 && length args1 == length args2 && all (uncurry match) (zip args1 args2)--Verifica se é a mesma função e se os argumentos podem ser unificados.                      
+                            match _ _ = False
+
+-- Recebe uma lista de substituições a serem realizadas e o termo onde as substituições serão realizadas.
+substituteAll :: [(String, Term)] -> Term -> Term
+substituteAll [] t = t
+substituteAll ((x, t):xs) t' = substituteAll xs (substitute (x, t) t')                                            --Realiza uma lista de substituições em um termo.
+       where
+              substitute (x, t) (Atom y) = Atom y
+              substitute (x, t) (Var y) = if x == y then t else Var y                                             --Se a variável for igual a x, retorna o termo t, senão retorna a variável.
+              substitute (x, t) (Var a :\== b)
+                     | a == x && t /= b = t :\== b
+                     | otherwise = Var a :\== b
+              substitute (x, t) (a :\== Var b)
+                     | b == x && t /= a = a :\== t
+                     | otherwise = a :\== Var b
+              substitute (x, t) (Func n args) = Func n (map (substitute (x, t)) args)                             --Aplica a substituição em cada argumento da função.
+
+-- Retorna uma lista de substituições a serem realizadas para que dois termos que deram match sejam unificados.
+unify :: Term -> Term -> Subst
+unify (Atom x) (Atom y) = if x == y then Just [] else Nothing
+unify (Var x) (Var a :\== Var b) = if Var x == Var a && Var x /= Var b then Just [] else Nothing
+unify (Atom x) (Var a :\== Var b) = Just [(a, Atom x)]
+unify (Atom x) (Var a :\== b) = if Atom x /= b then Just [(a, Atom x)] else Nothing
+unify (Func n args) (Atom a :\== Atom b) = if a == b then Nothing else Just [(a, Func n args)]                    -- quase, é algo próximo disto.
+unify (Func n args) (Var a :\== Var b) = Just [(a, Func n args)]
+unify t (Var x)  = Just [(x, t)]
+unify (Var x) t = unify t (Var x) 
+unify (Func n1 args1) (Func n2 args2) = if n1 == n2 && length args1 == length args2 then unifyList args1 args2 else Nothing
+       where
+              unifyList [] [] = Just []
+              unifyList (t:ts) (t':ts') = case unify t t' of
+                     Nothing -> Nothing
+                     Just subst -> case unifyList (map (substituteAll subst) ts) (map (substituteAll subst) ts') of
+                            Nothing -> Nothing
+                            Just subst' -> Just (subst ++ subst')
+              unifyList _ _  = Nothing
+
+--------------------------------------------------------------------------
+-- Exemplos de consultas interpretando o programa e exibindo o resultado--
+--------------------------------------------------------------------------
+
+main :: IO ()
+main = print (queryResult myExample (Func "consulta" [Var "Q"]))
+
 main1 :: IO ()
-main1 = print (queryResult myExample1 (Func "parent" [Var "X", Atom "koos"]))
+main1 = print (queryResult myExample1 (Func "parent" [Var "X", Atom "james"]))
 
 main2 :: IO ()
-main2 = print (queryResult myExample2 (Func "likes" [Atom "prolog", Var "X"]))
+main2 = print (queryResult myExample2 (Func "likes" [Atom "prolog", Var "Q"]))             -- aqui não funciona alpha conversion pq??
 
 main3 :: IO ()
-main3 = print (queryResult myExample3 (Func "irmao" [Var "X", Atom "ariel"]))
+main3 = print (queryResult myExample3 (Func "irmao" [Var "Q", Atom "ariel"]))              -- aqui funciona alpha conversion      
 
---Examples
+--------------------------------
+--Exemplos de programas Prolog--
+--------------------------------
+
+myExample :: Prolog
+myExample = [
+       Simple (Func "irmao" [Atom "andre", Atom "joao"]),
+       Func "consulta" [Var "X"] :- [Func "irmao" [Atom "andre", Var "X"]]]
 
        --Modern Compiler Design Example page 613
 myExample1 :: Prolog
@@ -165,7 +161,7 @@ myExample2 = [
        Simple (Func "likes" [Atom "claire", Atom "maths"]),
        Func "likes" [Var "X", Var "P"] :- [Func "based" [Var "P", Var "Y"], Func "likes" [Var "X", Var "Y"]]]
 
-       --Genealogic Tree Example --- olhar maribel, tudo que era x, vira Z, nas substituições
+       --Genealogic Tree Example
 myExample3 :: Prolog
 myExample3 = [
        Simple (Func "progenitor" [Atom "vitoria", Atom "joao"]),

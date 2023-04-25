ef915bdcb9d05cb408a3b5044891275188aca65c
Ari Vitor S. Lazzarotto <arivitordasilva@gmail.com>
2023-04-25 00:08:40 -0300

    one bug more.

--- Prolog_Library.hs
+++ Prolog_Library.hs
@@ -1,12 +1,11 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 
-import Data.List (nub, permutations) -- nub remove elementos duplicados de uma lista.
+import Data.List (nub, permutations) -- nub remove elementos duplicados de uma lista, permutations retorna todas as permutações de uma lista.
 
 -- https://downloads.haskell.org/~ghc/7.2.1/docs/html/users_guide/pragmas.html - 7.13.5.1. INLINE pragma - GHC User's Guide
 
--- bug que precisa permutar os argumentos da função para que a consulta funcione corretamente.
--- bug com a função mae e avo, porém pai funciona o.O (não sei o motivo)
+-- bug com a função mae e avo, porém pai funciona o.O (não sei o motivo) -- mae de janeti é a janeti? lul
 
 data Term = Var String | Atom String | Func String [Term]
        deriving (Eq,Show)
@@ -27,15 +26,15 @@ queryResult prog term = case interpret prog (alphaConvert term) of
        Just solution -> [head (result (Just solution))]
        where
               alphaConvert (Func n args) = Func n (map alphaConvert args)
-              alphaConvert (Var x) = Var (x ++ head [newName | i <- [1..], let newName = "VAR" ++ show i] )         --ou (x ++ "!@#$%%^&*_+")  
+              alphaConvert (Var x) = Var (x ++ head [newName | i <- [1..], let newName = "!@#$%%^&*_+VAR" ++ show i] )
               alphaConvert (Atom x) = Atom x 
               result Nothing = []
               result (Just subst) = [substituteAll subst (Var x) | (x, _) <- subst]                                 --Desencapsula o Maybe e retorna o resultado.
               queryResultAux prog term = case interpret prog (alphaConvert term) of
-                     Just subst -> [head (result (Just subst))]
                      Nothing -> []
+                     Just subst -> [head (result (Just subst))]
 
--- Percorre cada cláusula e aplica a unificação com o termo da consulta.
+-- Percorre cada cláusula que der match com o termo da consulta e aplica a unificação.
 interpret :: Prolog -> Term -> Subst
 interpret prog term = case query prog term of
        [] -> Nothing
@@ -46,7 +45,7 @@ interpret prog term = case query prog term of
                      Just subst' -> Just (subst ++ subst')
        where
               interpretList _ [] = Just []
-              interpretList prog (t:ts) = case interpret prog t of--Recursão que percorre cada termo da lista de termos da cláusula e aplica a unificação com o termo da consulta
+              interpretList prog (t:ts) = case interpret prog t of                                                 --Percorre cada termo da lista de termos da cláusula e aplica a unificação com o termo da consulta
                      Nothing -> Nothing
                      Just subst -> case interpretList prog (map (substituteAll subst) ts) of
                             Nothing -> Nothing
@@ -62,7 +61,7 @@ interpret prog term = case query prog term of
                             match _ (Var x) = True
                             match (Func n1 args1) (Func n2 args2) = n1 == n2 && length args1 == length args2 && all (uncurry match) (zip args1 args2)--Verifica se é a mesma função e se os argumentos podem ser unificados.
 
--- Retorna uma lista de substituições a serem realizadas para que dois termos que deram match sejam unificados.
+-- Retorna uma lista de substituições a serem realizadas para que dois termos sejam unificados.
 unify :: Term -> Term -> Subst
 unify (Atom x) (Atom y) = if x == y then Just [] else Nothing
 unify t (Var x) = Just [(x, t)]
@@ -76,7 +75,7 @@ unify (Func n1 args1) (Func n2 args2) = if n1 == n2 && length args1 == length ar
                             Nothing -> Nothing
                             Just subst' -> Just (subst ++ subst')
 
--- Recebe uma lista de substituições a serem realizadas e o termo onde as substituições serão realizadas.
+-- Recebe uma lista de substituições e o termo onde as substituições serão realizadas e retorna o termo com as substituições realizadas.
 substituteAll :: [(String, Term)] -> Term -> Term
 substituteAll [] t = t
 substituteAll ((x, t):xs) t' = substituteAll xs (substitute (x, t) t')                                            --Realiza uma lista de substituições em um termo.
@@ -90,7 +89,7 @@ substituteAll ((x, t):xs) t' = substituteAll xs (substitute (x, t) t')          
 ------------------------------------------------------------------------
 
 main :: IO ()
-main = print (queryResult myExample (Func "mae" [Var "Q", Atom "ariel"]))              -- consulta pai funciona
+main = print (queryResult myExample (Func "mae" [Var "Q", Atom "janeti"]))              -- consulta pai funciona
 
 main1 :: IO ()
 main1 = print (queryResult myExample1 (Func "consulta" [Var "Q"]))
@@ -99,7 +98,7 @@ main2 :: IO ()
 main2 = print (queryResult myExample2 (Func "parent" [Var "X", Atom "james"]))
 
 main3 :: IO ()
-main3 = print (queryResult myExample3 (Func "likes" [Atom "prolog", Var "Q"]))
+main3 = print (queryResult myExample3 (Func "likes" [Atom "prolog", Var "Y"]))
 
 --------------------------------
 --Exemplo de programa Prolog----
@@ -108,31 +107,45 @@ main3 = print (queryResult myExample3 (Func "likes" [Atom "prolog", Var "Q"]))
        --Genealogic Tree Example
 myExample :: Prolog
 myExample = [
-       Simple (Func "progenitor" [Atom "vitoria", Atom "joao"]),
-       Simple (Func "progenitor" [Atom "olicio", Atom "joao"]),
-       Simple (Func "progenitor" [Atom "vitoria", Atom "paulina"]),
-       Simple (Func "progenitor" [Atom "olicio", Atom "paulina"]),
        Simple (Func "progenitor" [Atom "joao", Atom "ari"]),
        Simple (Func "progenitor" [Atom "vitoria", Atom "ari"]),
        Simple (Func "progenitor" [Atom "olicio", Atom "janeti"]),
        Simple (Func "progenitor" [Atom "paulina", Atom "janeti"]),
        Simple (Func "progenitor" [Atom "ari", Atom "ari_vitor"]),
        Simple (Func "progenitor" [Atom "janeti", Atom "ari_vitor"]),
-       Simple (Func "progenitor" [Atom "ari", Atom "ariel"]),
-       Simple (Func "progenitor" [Atom "janeti", Atom "ariel"]),
        Simple (Func "sexo" [Atom "paulina", Atom "feminino"]),
        Simple (Func "sexo" [Atom "vitoria", Atom "feminino"]),
        Simple (Func "sexo" [Atom "janeti", Atom "feminino"]),
        Simple (Func "sexo" [Atom "ari", Atom "masculino"]),
        Simple (Func "sexo" [Atom "joao", Atom "masculino"]),
        Simple (Func "sexo" [Atom "olicio", Atom "masculino"]),
-       Simple (Func "sexo" [Atom "ariel", Atom "masculino"]),
        Simple (Func "sexo" [Atom "ari_vitor", Atom "masculino"]),
        Func "descendente" [Var "X", Var "Y"] :- [Func "progenitor" [Var "X", Var "Y"]],
        Func "descendente" [Var "X", Var "Y"] :- [Func "progenitor" [Var "X", Var "A"], Func "descendente" [Var "A", Var "Y"]],
                Func "avo" [Var "X", Var "Y"] :- [Func "progenitor" [Var "X", Var "A"], Func "progenitor" [Var "A", Var "Y"], Func "sexo" [Var "X", Atom "masculino"]],
                Func "mae" [Var "X", Var "Y"] :- [Func "progenitor" [Var "X", Var "Y"], Func "sexo" [Var "X", Atom "feminino"]],
                Func "pai" [Var "X", Var "Y"] :- [Func "progenitor" [Var "X", Var "Y"], Func "sexo" [Var "X", Atom "masculino"]]]
+
+myExample0 :: Prolog
+myExample0 = [
+       Simple (Func "progenitor" [Atom "joao", Atom "ari"]),
+       Simple (Func "progenitor" [Atom "vitoria", Atom "ari"]),
+       Simple (Func "progenitor" [Atom "olicio", Atom "janeti"]),
+       Simple (Func "progenitor" [Atom "paulina", Atom "janeti"]),
+       Simple (Func "progenitor" [Atom "ari", Atom "ari_vitor"]),
+       Simple (Func "progenitor" [Atom "janeti", Atom "ari_vitor"]),
+       Simple (Func "mulher" [Atom "paulina"]),
+       Simple (Func "mulher" [Atom "vitoria"]),
+       Simple (Func "mulher" [Atom "janeti"]),
+       Simple (Func "homen" [Atom "ari"]),
+       Simple (Func "homen" [Atom "joao"]),
+       Simple (Func "homen" [Atom "olicio"]),
+       Simple (Func "homen" [Atom "ari_vitor"]),
+       Func "descendente" [Var "X", Var "Y"] :- [Func "progenitor" [Var "X", Var "Y"]],
+       Func "descendente" [Var "X", Var "Y"] :- [Func "progenitor" [Var "X", Var "A"], Func "descendente" [Var "A", Var "Y"]],
+               Func "avo" [Var "X", Var "Y"] :- [Func "progenitor" [Var "X", Var "A"], Func "progenitor" [Var "A", Var "Y"], Func "homen" [Var "X"]],
+               Func "mae" [Var "X", Var "Y"] :- [Func "progenitor" [Var "X", Var "Y"], Func "mulher" [Var "X"]],
+               Func "pai" [Var "X", Var "Y"] :- [Func "progenitor" [Var "X", Var "Y"], Func "homen" [Var "X"]]]
 
 myExample1 :: Prolog
 myExample1 = [
